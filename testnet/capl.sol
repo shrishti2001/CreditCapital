340306c156970117312dc949d0155ceb07c90e6f
shrishti2001 <89570811+shrishti2001@users.noreply.github.com>
2021-10-01 18:31:56 +0530

    Create capl.sol

---
+++ testnet/capl.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract CAPL is Ownable, ERC20("CAPL Token", "CAPL") {
+    /**
+     * Mint New ERC20 Token
+     *
+     * @param amount - Amount of ERC20 tokens to be added
+     */
+    constructor(uint256 amount)  {
+        _mint(_msgSender(), amount);
+    }
+    
+    function minttoken(uint256 _amount) external onlyOwner{
+         _mint(_msgSender(), _amount);
+    }
+}
